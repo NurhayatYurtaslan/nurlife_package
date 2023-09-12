5e205d62c0a8eec5b9b24d524db1b05d37c78284
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-09-12 23:34:20 +0300

    Update nurlife_package.dart

--- lib/nurlife_package.dart
+++ lib/nurlife_package.dart
@@ -1,41 +1,66 @@
 import 'package:flutter/material.dart';
 
-class NurlifeWidget extends StatelessWidget {
-  final String text;
+class HeartAnimation extends StatefulWidget {
+  @override
+  _HeartAnimationState createState() => _HeartAnimationState();
+}
 
-  const NurlifeWidget({Key? key, required this.text}) : super(key: key);
+class _HeartAnimationState extends State<HeartAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _sizeAnimation;
+  late Animation<double> _opacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+
+    _sizeAnimation = Tween<double>(begin: 100, end: 200).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(0.0, 0.5, curve: Curves.easeInOut),
+      ),
+    );
+
+    _opacityAnimation = Tween<double>(begin: 1.0, end: 0.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(0.5, 1.0, curve: Curves.easeInOut),
+      ),
+    );
+
+    _animationController.repeat();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.bottomLeft,
-      child: Stack(
-        children: [
-          // Arka plan daire
-          const Positioned(
-            width: 50,
-            height: 50,
-            left: 0,
-            bottom: 0,
-            child: CircleAvatar(
-              backgroundColor: Colors.pink,
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return Opacity(
+          opacity: _opacityAnimation.value,
+          child: Container(
+            width: _sizeAnimation.value,
+            height: _sizeAnimation.value,
+            child: Icon(
+              Icons.favorite,
+              color: Colors.red,
+              size: _sizeAnimation.value,
             ),
           ),
-          // İçerik
-          Positioned(
-            bottom: 12,
-            left: 8,
-            child: Text(
-              text,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 12,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
